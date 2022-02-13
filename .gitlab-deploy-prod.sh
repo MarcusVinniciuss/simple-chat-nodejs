82d4f639dbce25fa5d125b5cddbc4b3027cd6ed5
Marcus Vinnicius <86576184+MarcusVinniciuss@users.noreply.github.com>
2022-02-13 17:35:53 -0300

    Up to data

--- .gitlab-deploy-prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -f
-string=$PROD_DEPLOY_SERVER
-array=(${string//,/})
-
-for i in "${!array[@]}"; do 
-    echo "Deploy project on server ${array[i]}"
-    ssh ec2-user@${array[i]} "forever"
